9387c7ca26228b4c2477a184d5d002a098b7235f
Casey Link <unnamedrambler@gmail.com>
2013-04-04 20:59:25 +0200

    type field gets synced to cartodb

--- cartodbutils/__init__.py
+++ cartodbutils/__init__.py
@@ -4,7 +4,7 @@ import datetime
 
 class CartoTransaction(object):
 
-    _SQL_INSERT = "insert into %s ( the_geom, happened_at, message ) values( %s, %s, %s);"
+    _SQL_INSERT = "insert into %s ( the_geom, type, happened_at, message ) values( %s, %s, %s, %s);"
 
     def __init__(self, api_key, domain, table, debug = False):
         self.cl = CartoDBAPIKey(api_key, domain)
@@ -24,7 +24,7 @@ class CartoTransaction(object):
         if self.debug:
             print resp
 
-    def _craft_insert(self, the_geom, happened_at, message):
+    def _craft_insert(self, the_geom, event_type, happened_at, message):
         if happened_at is None:
             happened_at = ''
         if message is None:
@@ -33,17 +33,17 @@ class CartoTransaction(object):
         def quote(s):
             return "'" + s + "'"
 
-        return self._SQL_INSERT % (self.table, the_geom , quote(happened_at), quote(message))
+        return self._SQL_INSERT % (self.table, the_geom , quote(event_type), quote(happened_at), quote(message))
 
     def insert_point(self, point):
         the_geom = "ST_SetSRID(ST_Point(%s,%s), 4326)" %(point.longitude, point.latitude)
-        insert = self._craft_insert(the_geom, point.dateTime, point.message)
+        insert = self._craft_insert(the_geom, "checkin", point.dateTime, point.message)
         self.queries.append(insert)
 
     def update_line(self, trip_id, coords):
         geojson = json.dumps({ "type" : "MultiLineString", "coordinates": [coords] })
         the_geom = "ST_SetSRID(ST_GeomFromGeoJSON('%s'), 4326)" % (geojson)
-        insert = self._craft_insert(the_geom, str(datetime.datetime.now()), None)
+        insert = self._craft_insert(the_geom, "track", str(datetime.datetime.now()), None)
         self.queries.append(insert)
 
 
