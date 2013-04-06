cc2815b6774ec7fe63c3e4b9da6dc753dce9680f
Casey Link <unnamedrambler@gmail.com>
2013-04-06 17:05:34 +0200

    don't return points in trips list

--- wander/app/api.py
+++ wander/app/api.py
@@ -4,5 +4,17 @@ from wander.app import app, db
 from models import Trip, Point
 manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
 
-manager.create_api(Trip, methods=['GET', 'POST', 'DELETE'])
+def cols_trip(params):
+
+    for o in params['objects']:
+        del o['points']
+    return params
+
+trip_post = {
+    "GET_MANY": [
+        cols_trip
+        ]
+    }
+
+manager.create_api(Trip, methods=['GET', 'POST', 'DELETE'], postprocessors=trip_post)
 manager.create_api(Point, methods=['GET'])
